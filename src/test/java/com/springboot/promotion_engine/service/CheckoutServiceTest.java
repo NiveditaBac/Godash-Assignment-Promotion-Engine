13d749d7789cd039cb5b4644a90096cc4de0d6af
NiveditaBac <niveditabachhar91@gmail.com>
2025-07-06 17:42:29 +0530

    Added test: scenario A for 1A + 1B + 1C

---
+++ src/test/java/com/springboot/promotion_engine/service/CheckoutServiceTest.java
@@ -0,0 +1,39 @@
+package com.springboot.promotion_engine.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.springboot.promotion_engine.entity.CartItem;
+import com.springboot.promotion_engine.entity.Sku;
+import com.springboot.promotion_engine.service.promotions.ComboPromotion;
+import com.springboot.promotion_engine.service.promotions.FixedPricePromotion;
+import com.springboot.promotion_engine.service.promotions.Promotion;
+
+public class CheckoutServiceTest {
+    private CheckoutService checkoutService;
+
+    @BeforeEach
+    void setup() {
+        List<Promotion> promotions = List.of(
+                new FixedPricePromotion('A', 3, 130),
+                new FixedPricePromotion('B', 2, 45),
+                new ComboPromotion('C', 'D', 30)
+        );
+        checkoutService = new CheckoutService(promotions);
+    }
+
+    @Test 
+    void testScenarioA() {
+
+        List<CartItem> items = List.of(
+                new CartItem(new Sku('A', 50), 1),
+                new CartItem(new Sku('B', 30), 1),
+                new CartItem(new Sku('C', 20), 1)
+        );
+        assertEquals(100, checkoutService.calculateTotal(items));
+    }
+}
